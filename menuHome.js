5974702c1bbb176ea6c704f54dbd1bdb5dac9268
aprendiendo-con-fer <floresv.mariaf@docente.sonora.edu.mx>
2026-02-21 20:42:15 -0700

    Create menuHome.js

---
+++ menuHome.js
@@ -0,0 +1,22 @@
+document.write(`
+<div style="
+position: fixed;
+top: 20px;
+right: 20px;
+z-index: 9999;
+">
+  <button onclick="window.location.href='index.html'"
+    style="
+    background: #111;
+    color: white;
+    border: none;
+    padding: 12px 14px;
+    border-radius: 50%;
+    font-size: 20px;
+    cursor: pointer;
+    box-shadow: 0 4px 10px rgba(0,0,0,0.3);
+    ">
+    🏠
+  </button>
+</div>
+`);
